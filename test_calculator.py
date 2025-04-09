56a51b80bf04bbe50caa620fe09ee06f0f6d8f2d
Mali M <you@example.com>
2025-04-09 18:04:26 -0400

    test4

--- test_calculator.py
+++ test_calculator.py
@@ -7,7 +7,9 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(add(1, 2), 3)
         self.assertEqual(add(2, 3), 5)
     def test_divide_by_zero(self):
-        self.assert(divide(15, 5), 3)
+        with self.assertRaises(ZeroDivisionError):
+            divide(0, 5)
+            raise ZeroDivisionError
     def test_log_invalid_base(self):
         self.assertRaises(ValueError, logarithm, 0, 0)
     def test_logarithm(self):
